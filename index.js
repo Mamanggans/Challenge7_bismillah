e0325b5dc8620ac6e2773b106c30e01d8dd0b248
ferdiansyah <ferdikings03@gmail.com>
2023-12-06 22:36:40 +0700

    sentry

--- index.js
+++ index.js
@@ -15,14 +15,18 @@ import * as Sentry from "@sentry/node";
 import express from "express";
 
 
+const Sentry = require("@sentry/node");
+
+
 Sentry.init({
   dsn: 'https://7c07740b45c2128ad45decea32294cc8@o4506321635180544.ingest.sentry.io/4506321639374848',
+  tracesSampleRate: 1.0,
 });
 
 // The request handler must be the first middleware on the app
 app.use(Sentry.Handlers.requestHandler());
 
-// All your controllers should live here
+
 app.get("/", function rootHandler(req, res) {
   res.end("Hello world!");
 });
